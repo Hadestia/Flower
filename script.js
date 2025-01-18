d5ddffb51cbd6cc44c30829e048b89b93bdfc330
Hadestia <rjchristianroldan@gmail.com>
2025-01-18 09:57:23 +0800

    updated file(s)

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+window.addEventListener("load", () => {
+	const bgm = document.getElementById("bgm");
+	
+	try {
+		bgm.play();
+	} catch (err) {
+		console.log(err);
+	}
+});
